d2859396d952eaddb259c089e461b7a841dd3fec
mizusashi <sakdfjidoie@yahoo.co.jp>
2021-06-26 10:11:22 +0900

    構造体

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,32 @@
+use rand::Rng;
 fn main() {
-    use rand::Rng;
-    let mut rng = rand::thread_rng();
     let char_set = String::from("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789");
-    let len = char_set.chars().count() as u32;
-    let pwlen = rng.gen_range(8,16+1);
+    let pwgen = PasswordGenerator::new(char_set, 8, 16);
+    println!("{}", pwgen.generator());
+}
 
-    for _n in 1..=pwlen {
-        let random_num: u32 = rng.gen_range(0, len);
-        print!("{}", char_set.chars().nth(random_num as usize).unwrap());
+struct PasswordGenerator {
+    len: u32,
+    char_set: String,
+}
+
+impl PasswordGenerator {
+    fn new(char_set: impl Into<String>, low: u32, high: u32) -> PasswordGenerator {
+        let mut rng = rand::thread_rng();
+        PasswordGenerator {
+            len: rng.gen_range(low, high + 1) as u32,
+            char_set: char_set.into(),
+        }
+    }
+
+    fn generator(&self) -> String {
+        let len = self.char_set.chars().count() as u32;
+        let mut random_string = String::new();
+        let mut rng = rand::thread_rng();
+        for _n in 1..=self.len {
+            let random_num: u32 = rng.gen_range(0, len);
+            random_string.push(self.char_set.chars().nth(random_num as usize).unwrap());
+        }
+        random_string
     }
 }
